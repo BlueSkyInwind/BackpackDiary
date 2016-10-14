8159f4c8287ad40df39a3d131374cad353cfc588
Wangyongxin <blueskyinwind@126.com>
2016-10-14 17:13:12 +0800

    first commit

---
+++ BackpackDiary/Public/commonuserful/commonuserful.swift
@@ -0,0 +1,18 @@
+//
+//  commonuserful.swift
+//  BackpackDiary
+//
+//  Created by Wangyongxin on 2016/10/14.
+//  Copyright © 2016年 Wangyongxin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SCREEN_WIDTH = UIScreen.main.bounds.size.width
+let SCREEN_HEIGHT = UIScreen.main.bounds.size.height
+
+
+
+
+
